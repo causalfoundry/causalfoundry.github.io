d54d64a3d036528c4de42b9e4ba60f2bcd37a7f8
jiayi <annajiayi@gmail.com>
2022-12-15 16:44:10 +0100

    chore: change text

--- src/components/Landing/Landing.vue
+++ src/components/Landing/Landing.vue
@@ -3,8 +3,8 @@
     <div>
       <transition name="fade" appear>
         <h1 class="landing__header">
-          <app-title>Democratizing</app-title>
-          Reinforcement Learning and Adaptive Experimentation
+          <app-title>Personalization</app-title>
+          to solve some of the greatest challenges in healthcare
         </h1>
       </transition>
       <transition name="fade" appear>
@@ -14,7 +14,7 @@
 
     <transition name="fade" appear>
       <div>
-        <img src="../../assets/pattern-circle.png" class="landing__pattern-circle" />
+        <img src="../../assets/pattern-circle.png" class="landing__pattern-circle" ref="floater" />
         <img src="../../assets/gradient-right-yellow.png" class="landing__gradient" />
       </div>
     </transition>
@@ -22,5 +22,24 @@
 </template>
 
 <script lang="ts" setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 import AppTitle from '@/components/AppTitle'
+
+const floater = ref()
+
+const parallax = () => {
+  if (floater.value) {
+    const yPos = 0 - window.scrollY / 80
+    console.log('before the top?', floater.value)
+    floater.value.style.top = `${yPos}%`
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', parallax)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', parallax)
+})
 </script>
