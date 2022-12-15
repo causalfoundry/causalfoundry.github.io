a7e36a30c697d9f2e7c20e4fa03f9286a5543dc3
jiayi <annajiayi@gmail.com>
2022-12-15 18:45:48 +0100

    chore: update styles and text

--- src/components/Proposal/Proposal.vue
+++ src/components/Proposal/Proposal.vue
@@ -2,10 +2,10 @@
   <section class="proposal">
     <section-header>Personalized Medicine</section-header>
     <div class="proposal__content">
-      <p>Our vision is to tailor early interventions — whether treatment or preventative in nature—to each individual patient.</p>
+      <p>Our vision is to tailor interventions — whether treatment or preventative in nature—to each individual patient.</p>
 
       <div>
-        <h3><app-title>Immunotherapy</app-title></h3>
+        <h3><app-title>{{ `Immunotherapy`.toUpperCase() }}</app-title></h3>
         <h4>Genomic and molecular profiling</h4>
         <p>Every patient has an individual mutational signature, with only a small fraction of mutations shared between patients. Only by a personalized approach, the full spectrum of mutations displayed by the tumor of a given patient can be leveraged to mobilize broad anti-tumor immunity.</p>
      </div>
