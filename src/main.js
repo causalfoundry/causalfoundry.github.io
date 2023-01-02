d160de178b0985e736a2b8b07e14b5e05a189056
jiayi <annajiayi@gmail.com>
2023-01-02 17:24:05 +0100

    feat: create dev-test route for testing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
-import { createApp } from 'vue'
+import { createApp, h } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './styles/app.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+export const app = createApp({
+  render: () => h(App)
+})
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'landing',
+      component: () => import('./views/Home')
+    },
+    {
+      path: '/dev-test',
+      name: 'dev-test',
+      component: () => import('./views/Test')
+    }
+]
+})
+
+app.use(router)
+app.mount('#app')
