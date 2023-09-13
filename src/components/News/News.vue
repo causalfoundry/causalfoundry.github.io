22e6e496e7b7b97eef389165b61af72f5e10c944
Andrés Estévez <aecostas@gmail.com>
2023-09-13 14:27:10 +0200

    Open links in new tab

--- src/components/News/News.vue
+++ src/components/News/News.vue
@@ -8,7 +8,7 @@
         class="news__content__item"
       >
         <div class="image">
-          <a :href="news_item.link">
+          <a :href="news_item.link" target="_blank">
             <img :src="`${news_item.image}`" />
           </a>
           <div v-if="news_item.credits" class="credits">
@@ -18,7 +18,9 @@
         <div class="date">{{ news_item.date }}</div>
         <div class="description" v-html="news_item.text"></div>
         <div class="link">
-          <a :href="news_item.link"> {{ news_item.linkText }} </a>
+          <a :href="news_item.link" target="_blank">
+            {{ news_item.linkText }}
+          </a>
         </div>
       </div>
     </div>
@@ -37,7 +39,7 @@ const news = [
     link: "https://twitter.com/Causal_Foundry/status/1701606748568822099?s=20",
     linkText: "Read more on X",
     date: dayjs("2023-09-12").format("DD MMM YYYY"),
-    text: `We're excited to be a recipient of the <a href="http://Google.org">Google.org</a> AI for the Global Goals Grant Portfolio.
+    text: `We're excited to be a recipient of the <a href="http://Google.org" target="_blank">Google.org</a> AI for the Global Goals Grant Portfolio.
            Causal Foundry will build an AI-enabled patient care application to support frontline workers
            in Africa. Learn more about the other recipients here:
            <a href="http://g.co/ai/globalgoals">http://g.co/ai/globalgoals </a>`,
@@ -49,7 +51,7 @@ const news = [
     linkText: "Read more on Linkedin",
     date: dayjs("2023-08-04").format("DD MMM YYYY"),
     text: `We are thrilled to work with
-    <a href="https://www.linkedin.com/company/aide-chemists-ltd/">AIDE CHEMISTS LTD</a>
+    <a href="https://www.linkedin.com/company/aide-chemists-ltd/" target="_blank">AIDE CHEMISTS LTD</a>
      to build together an AI-native e-pharmacy application
       to revolutionize access to medications and medical advice.`,
     image: "/images/news/CFxAC1_1.png",
@@ -58,7 +60,7 @@ const news = [
     link: "https://www.linkedin.com/posts/medtronic-labs_medtronic-labs-causal-foundry-revolutionizing-activity-7052612498365038592-stCO",
     linkText: "Read more on Linkedin",
     date: dayjs("2023-04-15").format("DD MMM YYYY"),
-    text: `Excited to announce our partnership with <a href="https://www.medtroniclabs.org/">Medtronic Labs</a>. 
+    text: `Excited to announce our partnership with <a href="https://www.medtroniclabs.org/" target="_blank">Medtronic Labs</a>. 
            Our goal is to work together to revolutionize healthcare delivery
            models in low and middle-income countries through AI and data-driven insights`,
     image: "/images/news/medtronics.png",
