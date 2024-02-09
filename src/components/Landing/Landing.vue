ee7854ebf6bcb2c499772a58ba115b3994d65919
Arillaxe <bekirshabut66@gmail.com>
2024-02-09 15:41:50 +0300

    Testing

--- src/components/Landing/Landing.vue
+++ src/components/Landing/Landing.vue
@@ -3,7 +3,7 @@
     <div>
       <transition name="fade" appear>
         <h1 class="landing__header">
-          <app-title>Personalization</app-title>
+          <app-title>Personalization DEV</app-title>
           to solve the greatest <br />challenges in healthcare
         </h1>
       </transition>
