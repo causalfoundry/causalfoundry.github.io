582cf2101a126f5aa1a6decb39fecd0a3005fb55
Arillaxe <bekirshabut66@gmail.com>
2024-02-20 01:40:45 +0300

    update about us images on mobile

--- src/pages/About/About.vue
+++ src/pages/About/About.vue
@@ -51,13 +51,21 @@
               <div class="name">{{ member.name }}</div>
               <div class="role">{{ member.role }}</div>
               <div class="links">
-                <a v-if="member.links.linkedin" :href="member.links.linkedin">
+                <a
+                  v-if="member.links.linkedin"
+                  :href="member.links.linkedin"
+                  target="_blank"
+                >
                   <img
                     src="../../assets/linkedin-about.png"
                     class="link linkedin"
                   />
                 </a>
-                <a v-if="member.links.twitter" :href="member.links.twitter">
+                <a
+                  v-if="member.links.twitter"
+                  :href="member.links.twitter"
+                  target="_blank"
+                >
                   <img
                     src="../../assets/twitter-about.png"
                     class="link twitter"
@@ -95,13 +103,7 @@ const team = [
     imageUrl: "/src/assets/africa.png",
     name: "África Periáñez",
     role: "CEO & Founder",
-    descritpion: `Sumiko is the COO at Causal Foundry. She brings extensive experience in
-      working globally with international institutions, nonprofit
-      organizations, and corporate businesses. Her focus includes ensuring
-      the effective implementation of projects and programs, and the successful
-      management of partnerships. She holds a BA in International Relations
-      from the University of Washington and a dual master’s degree in strategic
-      negotiations at Paris-Sud University.`,
+    descritpion: `África Periáñez, CEO and co-founder of Causal Foundry, is an AI entrepreneur dedicated to addressing health inequalities in low- and middle-income countries using data and machine learning. With a Ph.D. in Mathematics and extensive experience, including roles at Inditex and founding benshi.ai and Yokozuna Data, she leads the development of AI products tailored for healthcare professionals and patients. África's expertise spans from academia to industry, with a focus on personalized interventions and robust data collection to drive positive change.`,
     links: {
       linkedin: "https://linkedin.com",
       twitter: "https://twitter.com",
@@ -111,13 +113,7 @@ const team = [
     imageUrl: "/src/assets/dexian.png",
     name: "Dexian Tang",
     role: "CTO & Co-Founder",
-    descritpion: `Sumiko is the COO at Causal Foundry. She brings extensive experience in
-      working globally with international institutions, nonprofit
-      organizations, and corporate businesses. Her focus includes ensuring
-      the effective implementation of projects and programs, and the successful
-      management of partnerships. She holds a BA in International Relations
-      from the University of Washington and a dual master’s degree in strategic
-      negotiations at Paris-Sud University.`,
+    descritpion: `África Periáñez, CEO and co-founder of Causal Foundry, is an AI entrepreneur dedicated to addressing health inequalities in low- and middle-income countries using data and machine learning. With a Ph.D. in Mathematics and extensive experience, including roles at Inditex and founding benshi.ai and Yokozuna Data, she leads the development of AI products tailored for healthcare professionals and patients. África's expertise spans from academia to industry, with a focus on personalized interventions and robust data collection to drive positive change.`,
     links: {
       twitter: "https://twitter.com",
     },
@@ -126,13 +122,7 @@ const team = [
     imageUrl: "/src/assets/africa.png",
     name: "Sumiko Tanaka Pusch",
     role: "COO",
-    descritpion: `Sumiko is the COO at Causal Foundry. She brings extensive experience in
-      working globally with international institutions, nonprofit
-      organizations, and corporate businesses. Her focus includes ensuring
-      the effective implementation of projects and programs, and the successful
-      management of partnerships. She holds a BA in International Relations
-      from the University of Washington and a dual master’s degree in strategic
-      negotiations at Paris-Sud University.`,
+    descritpion: `Sumiko is the COO at Causal Foundry. She brings extensive experience in working globally with international institutions, nonprofit organizations, and corporate businesses. Her focus includes ensuring the effective implementation of projects and programs, and the successful management of partnerships. She holds a BA in International Relations from the University of Washington and a dual master’s degree in strategic negotiations at Paris-Sud University..`,
     links: {
       linkedin: "https://linkedin.com",
     },
@@ -141,13 +131,7 @@ const team = [
     imageUrl: "/src/assets/enric.png",
     name: "Enric Jané",
     role: "CSO",
-    descritpion: `Sumiko is the COO at Causal Foundry. She brings extensive experience in
-      working globally with international institutions, nonprofit
-      organizations, and corporate businesses. Her focus includes ensuring
-      the effective implementation of projects and programs, and the successful
-      management of partnerships. She holds a BA in International Relations
-      from the University of Washington and a dual master’s degree in strategic
-      negotiations at Paris-Sud University.`,
+    descritpion: `África Periáñez, CEO and co-founder of Causal Foundry, is an AI entrepreneur dedicated to addressing health inequalities in low- and middle-income countries using data and machine learning. With a Ph.D. in Mathematics and extensive experience, including roles at Inditex and founding benshi.ai and Yokozuna Data, she leads the development of AI products tailored for healthcare professionals and patients. África's expertise spans from academia to industry, with a focus on personalized interventions and robust data collection to drive positive change.`,
     links: {
       linkedin: "https://linkedin.com",
     },
@@ -156,13 +140,7 @@ const team = [
     imageUrl: "/src/assets/africa.png",
     name: "Ana Fernández del Río",
     role: "Principal Data Scientist",
-    descritpion: `Sumiko is the COO at Causal Foundry. She brings extensive experience in
-      working globally with international institutions, nonprofit
-      organizations, and corporate businesses. Her focus includes ensuring
-      the effective implementation of projects and programs, and the successful
-      management of partnerships. She holds a BA in International Relations
-      from the University of Washington and a dual master’s degree in strategic
-      negotiations at Paris-Sud University.`,
+    descritpion: `África Periáñez, CEO and co-founder of Causal Foundry, is an AI entrepreneur dedicated to addressing health inequalities in low- and middle-income countries using data and machine learning. With a Ph.D. in Mathematics and extensive experience, including roles at Inditex and founding benshi.ai and Yokozuna Data, she leads the development of AI products tailored for healthcare professionals and patients. África's expertise spans from academia to industry, with a focus on personalized interventions and robust data collection to drive positive change.`,
     links: {
       linkedin: "https://linkedin.com",
     },
@@ -171,13 +149,7 @@ const team = [
     imageUrl: "/src/assets/ruth.png",
     name: "Ruth Barry",
     role: "Operations Manager",
-    descritpion: `Sumiko is the COO at Causal Foundry. She brings extensive experience in
-      working globally with international institutions, nonprofit
-      organizations, and corporate businesses. Her focus includes ensuring
-      the effective implementation of projects and programs, and the successful
-      management of partnerships. She holds a BA in International Relations
-      from the University of Washington and a dual master’s degree in strategic
-      negotiations at Paris-Sud University.`,
+    descritpion: `África Periáñez, CEO and co-founder of Causal Foundry, is an AI entrepreneur dedicated to addressing health inequalities in low- and middle-income countries using data and machine learning. With a Ph.D. in Mathematics and extensive experience, including roles at Inditex and founding benshi.ai and Yokozuna Data, she leads the development of AI products tailored for healthcare professionals and patients. África's expertise spans from academia to industry, with a focus on personalized interventions and robust data collection to drive positive change.`,
     links: {
       linkedin: "https://linkedin.com",
     },
