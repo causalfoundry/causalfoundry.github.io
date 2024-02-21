daccff05db3320e115bf837c27fc05627b6a5ad6
Arillaxe <bekirshabut66@gmail.com>
2024-02-21 19:23:20 +0300

    Update team info

--- src/data/team.ts
+++ src/data/team.ts
@@ -5,53 +5,53 @@ export default [
     role: "CEO & Founder",
     descritpion: `África Periáñez, CEO and co-founder of Causal Foundry, is an AI entrepreneur dedicated to addressing health inequalities in low- and middle-income countries using data and machine learning. With a Ph.D. in Mathematics and extensive experience, including roles at Inditex and founding benshi.ai and Yokozuna Data, she leads the development of AI products tailored for healthcare professionals and patients. África's expertise spans from academia to industry, with a focus on personalized interventions and robust data collection to drive positive change.`,
     links: {
-      linkedin: "https://linkedin.com",
-      twitter: "https://twitter.com",
+      linkedin: "https://www.linkedin.com/in/africaperianez/",
+      twitter: "https://twitter.com/aperianez/",
     },
   },
   {
     imageUrl: "/images/team/dexian.png",
     name: "Dexian Tang",
     role: "CTO & Co-Founder",
-    descritpion: `África Periáñez, CEO and co-founder of Causal Foundry, is an AI entrepreneur dedicated to addressing health inequalities in low- and middle-income countries using data and machine learning. With a Ph.D. in Mathematics and extensive experience, including roles at Inditex and founding benshi.ai and Yokozuna Data, she leads the development of AI products tailored for healthcare professionals and patients. África's expertise spans from academia to industry, with a focus on personalized interventions and robust data collection to drive positive change.`,
+    descritpion: `Dexian is a software and distributed systems engineer, previously a senior machine learning engineer at Inditex, where he led the design of the Zara Brain AI platform for predicting customer behavior and optimizing supply chains. His prior role at Yokozuna Data involved scaling data pipelines and optimizing machine learning processes in the video game industry. Dexian has a background in electrical engineering, conducting research at the Okada Lab in the Tokyo Institute of Technology, resulting in publications and an Excellence Award from the IEICE Electronics Society. He holds an MEng in Electrical Engineering from the Tokyo Institute of Technology and a BEng from the University of Electronic Science and Technology of China.`,
     links: {
-      twitter: "https://twitter.com",
+      linkedin: "https://www.linkedin.com/in/dexian-tang/",
     },
   },
   {
-    imageUrl: "/images/team/africa.png",
+    imageUrl: "/images/team/sumiko.png",
     name: "Sumiko Tanaka Pusch",
     role: "COO",
     descritpion: `Sumiko is the COO at Causal Foundry. She brings extensive experience in working globally with international institutions, nonprofit organizations, and corporate businesses. Her focus includes ensuring the effective implementation of projects and programs, and the successful management of partnerships. She holds a BA in International Relations from the University of Washington and a dual master’s degree in strategic negotiations at Paris-Sud University..`,
     links: {
-      linkedin: "https://linkedin.com",
+      linkedin: "https://www.linkedin.com/in/sumiko-tanaka-pusch/",
     },
   },
   {
     imageUrl: "/images/team/enric.png",
     name: "Enric Jané",
     role: "CSO",
-    descritpion: `África Periáñez, CEO and co-founder of Causal Foundry, is an AI entrepreneur dedicated to addressing health inequalities in low- and middle-income countries using data and machine learning. With a Ph.D. in Mathematics and extensive experience, including roles at Inditex and founding benshi.ai and Yokozuna Data, she leads the development of AI products tailored for healthcare professionals and patients. África's expertise spans from academia to industry, with a focus on personalized interventions and robust data collection to drive positive change.`,
+    descritpion: `Dr. Enric Jané has over 15 years of experience in global health, working in consulting, academic, and donor organizations. Currently involved with the Bill & Melinda Gates Foundation, Gates Ventures, and Terre des hommes, he focuses on innovative projects to enhance health systems in low- and middle-income countries. In his recent role at the Gates Foundation, Dr. Jané worked on improving primary health care, expanding community health worker systems, utilizing information technologies, and increasing funding for child survival strategies. With a diverse background, including roles in Mozambique and Spain, Dr. Jané holds an MD, BSc in Physics, PhD from the University of Barcelona, and an MSc in Biomedical Engineering from Columbia University.`,
     links: {
-      linkedin: "https://linkedin.com",
+      linkedin: "https://www.linkedin.com/in/enricjane/",
     },
   },
   {
-    imageUrl: "/images/team/africa.png",
+    imageUrl: "/images/team/ana.png",
     name: "Ana Fernández del Río",
     role: "Principal Data Scientist",
-    descritpion: `África Periáñez, CEO and co-founder of Causal Foundry, is an AI entrepreneur dedicated to addressing health inequalities in low- and middle-income countries using data and machine learning. With a Ph.D. in Mathematics and extensive experience, including roles at Inditex and founding benshi.ai and Yokozuna Data, she leads the development of AI products tailored for healthcare professionals and patients. África's expertise spans from academia to industry, with a focus on personalized interventions and robust data collection to drive positive change.`,
+    descritpion: `Ana, a senior data scientist with over 15 years of experience, specializes in theoretical and statistical physics, particularly in human behavior modeling. She has worked with major companies like Coca-Cola, Telefónica, and Inditex, the world's largest fashion retailer. Ana holds a Ph.D. in Science, focusing on statistical modeling of human behavior, and has a strong background in machine learning algorithms. Currently with benshi, she previously worked with Yokozuna Data in Japan, where she collaborated with colleagues now at benshi.`,
     links: {
-      linkedin: "https://linkedin.com",
+      linkedin: "https://www.linkedin.com/in/ana-fernandez-del-rio/",
     },
   },
   {
     imageUrl: "/images/team/ruth.png",
     name: "Ruth Barry",
     role: "Operations Manager",
-    descritpion: `África Periáñez, CEO and co-founder of Causal Foundry, is an AI entrepreneur dedicated to addressing health inequalities in low- and middle-income countries using data and machine learning. With a Ph.D. in Mathematics and extensive experience, including roles at Inditex and founding benshi.ai and Yokozuna Data, she leads the development of AI products tailored for healthcare professionals and patients. África's expertise spans from academia to industry, with a focus on personalized interventions and robust data collection to drive positive change.`,
+    descritpion: `Ruth is an experienced Marketing and Operations professional with a diverse background in both Barcelona and the UK. She served as the Operations Manager for the European office of Partners for Youth Empowerment, where she developed partnerships for international training events. Originally from London, Ruth has worked with companies like BBC and Orange, and she also managed partnerships for a fashion start-up before relocating to Spain over a decade ago. In her free time, she volunteers in English teaching and supports women entrepreneurs. Ruth enjoys the scenic Catalan countryside, learning the guitar, and participating in a community choir.`,
     links: {
-      linkedin: "https://linkedin.com",
+      linkedin: "https://www.linkedin.com/in/barryruth/",
     },
   },
 ];
