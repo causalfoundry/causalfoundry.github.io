821a2f8b18d42d6a36c1e9b7c9f587571d15221d
moizhassankh <62547067+moizhassankh@users.noreply.github.com>
2024-09-17 16:09:38 +0200

    🛠️ chore: updated careers page

--- src/pages/Careers/Careers.vue
+++ src/pages/Careers/Careers.vue
@@ -2,22 +2,23 @@
   <div class="careers">
     <div class="container">
       <img
-        src="../../assets/gradient-top-yellow.png"
-        class="app__gradient-top-yellow"
+          src="../../assets/gradient-top-yellow.png"
+          class="app__gradient-top-yellow"
       />
       <img
-        src="../../assets/gradient-top-blue.png"
-        class="app__gradient-top-blue"
+          src="../../assets/gradient-top-blue.png"
+          class="app__gradient-top-blue"
       />
 
       <app-header current-page="careers">
-        <app-menu />
+        <app-menu/>
       </app-header>
 
       <!-- <intersection-observer @intersect="handleIntersect(Section.Jumbotron, $event)"> -->
       <div class="jumbotron">
         <div class="jumbotron__title">
-          <app-title>AI for Equity:</app-title> Build the Unbuilt with Us
+          <app-title>AI for Equity:</app-title>
+          Build the Unbuilt with Us
         </div>
         <div class="jumbotron__description">
           We are at the forefront of crafting advanced machine learning software
@@ -25,7 +26,7 @@
           future where technology empowers all
         </div>
         <div class="circle top">
-          <img src="../../assets/pattern-circle.png" alt="" />
+          <img src="../../assets/pattern-circle.png" alt=""/>
         </div>
       </div>
       <!-- </intersection-observer> -->
@@ -34,61 +35,78 @@
       <div class="jobs">
         <div class="jobs__title">Current Job Openings</div>
 
-        <!-- <div class="job" v-for="job in jobs">
-            <div class="job__titles">
-              <div class="job__title">{{ job.title }}</div>
-              <div class="job__sub-title">{{ job.subTitle }}</div>
-            </div>
-
-            <div class="job__info">
-              <div class="job__description">{{ job.description }}</div>
-              <router-link :to="`/careers/${job.key}`"><div class="job__link">Learn more</div></router-link>
-            </div>
-          </div> -->
-
         <div class="job">
           <div class="job__titles">
-            <div class="job__title">Senior Software Engineer (Data/ML)</div>
+            <div class="job__title">Machine Learning Scientist</div>
             <div class="job__sub-title">Barcelona, Full-time</div>
           </div>
 
           <div class="job__info">
             <div class="job__description">
-              We are looking for software engineers with a solid Go/SQL/Python
-              background to join our engineering team to build our data
-              infrastructure. You will have the rare opportunity to build
-              resilient and scalable machine learning products from scratch,
-              together with other talented engineers, to achieve equity in
-              health for all people worldwide via cutting-edge ML algorithms.
+              We seek machine learning scientists to push the boundaries of artificial intelligence in global health
+              including its application in low-income settings. We are interested in individuals with solid knowledge
+              and experience in developing and evaluating different architectures and algorithms, and their applications
+              in experimentation and causal inference. We value positively those who have research experience. You will
+              have the unique opportunity of, together with our data science and engineering teams, defining and
+              building features for our machine learning platform for equitable healthcare.
             </div>
             <a
-              target="_blank"
-              href="https://candy-silica-cf0.notion.site/Senior-Software-Engineer-Data-ML-c25aa672855c42c7b079f938d0a8fd11"
-              ><div class="job__link">Learn more</div></a
+                target="_blank"
+                href="https://causalfoundry.notion.site/Machine-Learning-Scientist-91b9e5aa4e2f48b09e689ff0533f0632"
+            >
+              <div class="job__link">Learn more</div>
+            </a
             >
           </div>
         </div>
 
         <div class="job">
           <div class="job__titles">
-            <div class="job__title">Machine Learning Engineer</div>
+            <div class="job__title">Back-end Engineer</div>
             <div class="job__sub-title">Barcelona, Full-time</div>
           </div>
 
           <div class="job__info">
             <div class="job__description">
-              We seek for machine learning and software engineers with a solid
-              Python/SQL background to join our engineering team to build our
-              machine learning and data infrastructure. You will have the rare
-              opportunity to build resilient and scalable machine learning
-              products from scratch, together with other talented engineers, to
-              achieve equity in health for all people worldwide via cutting-edge
-              ML algorithms.
+              We seek Back-end developers to join our engineering team. We are looking for hardworking and reliable
+              individuals with strong software developer backgrounds, and who are knowledgeable about building scalable
+              and robust architecture, tools, data products and critical data pipelines optimized for ML applications.
+              You will have the rare opportunity of building resilient and scalable machine learning products from
+              scratch, together with other talented engineers, to boost global health via cutting-edge machine learning
+              solutions.
             </div>
             <a
-              target="_blank"
-              href="https://candy-silica-cf0.notion.site/Machine-Learning-Engineer-6e096a586ca04ac79c764f8ca29d8936"
-              ><div class="job__link">Learn more</div></a
+                target="_blank"
+                href="https://causalfoundry.notion.site/Back-end-Engineer-1030c2a38a7e80c6a74efe3ac35c791a"
+            >
+              <div class="job__link">Learn more</div>
+            </a
+            >
+          </div>
+        </div>
+
+        <div class="job">
+          <div class="job__titles">
+            <div class="job__title">Mobile Engineer (React Native)</div>
+            <div class="job__sub-title">Barcelona, Full-time</div>
+          </div>
+
+          <div class="job__info">
+            <div class="job__description">
+              We are seeking an experienced React Native Developer to focus on SDK and app development, driving the
+              integration of our machine-learning products with partner applications. In this role, you will design and
+              optimize SDKs, ensuring performance, scalability, and seamless integration, while also building
+              high-quality, responsive applications for multiple platforms. This is a unique opportunity to work on both
+              SDK and app development, collaborating closely with cross-functional teams to deliver innovative,
+              impactful solutions in the Global Health space. Prior experiences working in a similar role with start-ups
+              is a strong plus.
+            </div>
+            <a
+                target="_blank"
+                href="https://causalfoundry.notion.site/Mobile-Engineer-React-Native-2f9bd200ae354dbabe068eae69f78f8c"
+            >
+              <div class="job__link">Learn more</div>
+            </a
             >
           </div>
         </div>
@@ -98,9 +116,9 @@
       <div class="careers__culture">
         Discover Our
         <a
-          target="_blank"
-          href="https://candy-silica-cf0.notion.site/Our-Interview-Process-1e8f13ac941043e59dd28506712a967f"
-          >Interview Approach and Culture Overview</a
+            target="_blank"
+            href="https://causalfoundry.notion.site/Our-Interview-Process-1040c2a38a7e8017bbc4c8fbb3d1e085"
+        >Interview Approach and Culture Overview</a
         >
         <div class="careers__culture__info">
           To apply, please email us your CV in PDF format along with a brief
@@ -110,18 +128,18 @@
       </div>
 
       <div class="pattern-wave">
-        <img src="../../assets/pattern-wave.png" />
+        <img src="../../assets/pattern-wave.png"/>
       </div>
 
-      <app-footer />
+      <app-footer/>
 
       <img
-        src="../../assets/gradient-bottom-yellow.png"
-        class="app__gradient-bottom-yellow"
+          src="../../assets/gradient-bottom-yellow.png"
+          class="app__gradient-bottom-yellow"
       />
       <img
-        src="../../assets/gradient-bottom-blue.png"
-        class="app__gradient-bottom-blue"
+          src="../../assets/gradient-bottom-blue.png"
+          class="app__gradient-bottom-blue"
       />
     </div>
   </div>
@@ -132,70 +150,6 @@ import AppHeader from "@/components/AppHeader";
 import AppMenu from "@/components/AppMenu";
 import AppFooter from "@/components/AppFooter";
 import AppTitle from "@/components/AppTitle";
-// import { onBeforeUnmount, onMounted, ref } from 'vue';
-// import jobs from '../../data/jobs';
-
 import "./Careers.scss";
 
-// enum Section {
-//   Jumbotron = "jumbotron",
-//   Jobs = "jobs",
-// }
-
-// const isSectionVisible = ref({
-//   [Section.Jumbotron]: true,
-//   [Section.Jobs]: false,
-// });
-
-// const handleIntersect = (
-//   section: Section,
-//   intersectEvent: IntersectionObserverEntry
-// ) => {
-//   if (intersectEvent.isIntersecting) {
-//     document.querySelector(`.${section}`)?.classList.add("show");
-//     document.querySelector(`.${section}`)?.classList.remove("hide");
-
-//     isSectionVisible.value[section] = true;
-//     return;
-//   }
-
-//   document.querySelector(`.${section}`)?.classList.add("hide");
-//   document.querySelector(`.${section}`)?.classList.remove("show");
-
-//   isSectionVisible.value[section] = false;
-// };
-
-// const parallax = () => {
-//   const els = document.querySelectorAll<HTMLElement>(".parallax");
-
-//   for (let i = 0; i < els.length; i++) {
-//     const matchedParallaxSection = els[i].classList.value
-//       .match(/parallax--(.*)/g)?.[0]
-//       .split("--")[1] as Section;
-//     const currentElScrollHeight =
-//       document.querySelector(`.${matchedParallaxSection}`)?.scrollHeight || 0;
-
-//     if (matchedParallaxSection === Section.Jumbotron) {
-//       const yPos = 0 - window.scrollY / 45;
-//       els[i].style.top = `${yPos}%`;
-//     }
-
-//     if (
-//       isSectionVisible.value[matchedParallaxSection] &&
-//       currentElScrollHeight <= window.scrollY
-//     ) {
-//       const root = window.scrollY - currentElScrollHeight;
-//       const yPos = 0 - root / 45;
-//       els[i].style.top = `${yPos}%`;
-//     }
-//   }
-// };
-
-// onMounted(() => {
-//   window.addEventListener("scroll", parallax);
-// });
-
-// onBeforeUnmount(() => {
-//   window.removeEventListener("scroll", parallax);
-// });
 </script>
