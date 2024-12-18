ffbc5b9f5c9951cdae2c518f93d99dafdd416167
Andrés Estévez <aecostas@gmail.com>
2024-12-18 13:51:01 +0100

    Test commit

--- src/components/AppFooter/AppFooter.vue
+++ src/components/AppFooter/AppFooter.vue
@@ -62,7 +62,7 @@
       <a class="footer__email" :href="`mailto:${EMAIL}`">{{ EMAIL }}</a>
     </div>
 
-    <p class="footer__copyright__mobile">© 2024 CausalFoundry, Inc.</p>
+    <p class="footer__copyright__mobile">© 2024 CausalFoundry</p>
   </footer>
 </template>
 
