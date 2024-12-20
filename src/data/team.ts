332ddb0ee71271af3cffa3b1158dac320b4baff0
moizhassankh <62547067+moizhassankh@users.noreply.github.com>
2024-12-20 19:34:34 +0100

    🎲 chore: add Sumiko profile

--- src/data/team.ts
+++ src/data/team.ts
@@ -1,33 +1,42 @@
 export default [
-  {
-    imageUrl: "/images/team/africa.png",
-    name: "África Periáñez",
-    role: "CEO & Founder",
-    descritpion: `África is an AI scientist and entrepreneur. África held the role of Chief Analytics Officer at Inditex, leading the AI strategy and bolstering one of the world's fastest supply chains. Previously, she founded Yokozuna Data, an AI firm based in Tokyo to transform videogame industry. África holds a PhD in Mathematics from the University of Reading and Master's degrees in String Theory and Theoretical Physics from CERN and UAM. She served as a Marie Curie EU research fellow at CERN and worked as a scientist at RIKEN and the German Weather Service. África has co-authored over 25 articles.`,
-    links: {
-      linkedin: "https://www.linkedin.com/in/africaperianez/",
-      twitter: "https://twitter.com/aperianez/",
+    {
+        imageUrl: "/images/team/africa.png",
+        name: "África Periáñez",
+        role: "CEO & Founder",
+        descritpion: `África is an AI scientist and entrepreneur. África held the role of Chief Analytics Officer at Inditex, leading the AI strategy and bolstering one of the world's fastest supply chains. Previously, she founded Yokozuna Data, an AI firm based in Tokyo to transform videogame industry. África holds a PhD in Mathematics from the University of Reading and Master's degrees in String Theory and Theoretical Physics from CERN and UAM. She served as a Marie Curie EU research fellow at CERN and worked as a scientist at RIKEN and the German Weather Service.`,
+        links: {
+            linkedin: "https://www.linkedin.com/in/africaperianez/",
+            twitter: "https://twitter.com/aperianez/",
+        },
     },
-  },
-  {
-    imageUrl: "/images/team/dexian.png",
-    name: "Dexian Tang",
-    role: "CTO & Co-Founder",
-    descritpion: `Dexian is a software and distributed systems engineer, previously a senior engineer at Inditex. His prior role in Tokyo, involved scaling data pipelines and AI processes in the video game industry. Dexian has a background in electrical engineering, conducting research at the Okada Lab in the Tokyo Institute of Technology, resulting in several publications and an Excellence Award from the IEICE Electronics Society. He holds an MEng in Electrical Engineering from the Tokyo Institute of Technology and a BEng from the University of Electronic Science and Technology of China.`,
-    links: {
-      linkedin: "https://www.linkedin.com/in/dexian-tang/",
-      github: "https://github.com/dexianta",
+    {
+        imageUrl: "/images/team/dexian.png",
+        name: "Dexian Tang",
+        role: "CTO & Co-Founder",
+        descritpion: `Dexian is a software engineer, previously a senior engineer at Inditex. His prior role in Tokyo, involved scaling data pipelines and AI processes in the video game industry. Dexian has a background in electrical engineering, researching at the Okada Lab in the Tokyo Institute of Technology, resulting in several publications and an Excellence Award from the IEICE Electronics Society. He holds an MEng in Electrical Engineering from the Tokyo Institute of Technology and a BEng from the University of Electronic Science and Technology of China.`,
+        links: {
+            linkedin: "https://www.linkedin.com/in/dexian-tang/",
+            github: "https://github.com/dexianta",
+        },
     },
-  },
-  {
-    imageUrl: "/images/team/enric.png",
-    name: "Enric Jané",
-    role: "CSO",
-    descritpion: `Dr. Enric Jané has over 15 years of experience in global health. In his previous role at the Gates Foundation, Dr. Jané worked on improving primary health care, expanding community health worker systems, utilizing information technologies, and increasing funding for child survival strategies. With a diverse background, including roles in Mozambique and Spain, Dr. Jané holds an MD, BSc in Physics, PhD from the University of Barcelona, and an MSc in Biomedical Engineering from Columbia University.`,
-    links: {
-      linkedin: "https://www.linkedin.com/in/enricjane/",
+    {
+        imageUrl: "/images/team/enric.png",
+        name: "Enric Jané",
+        role: "CSO",
+        descritpion: `Dr. Enric Jané has over 15 years of experience in global health. In his previous role at the Gates Foundation, Dr. Jané worked on improving primary health care, expanding community health worker systems, utilizing information technologies, and increasing funding for child survival strategies. With a diverse background, including roles in Mozambique and Spain, Dr. Jané holds an MD, BSc in Physics, PhD from the University of Barcelona, and an MSc in Biomedical Engineering from Columbia University.`,
+        links: {
+            linkedin: "https://www.linkedin.com/in/enricjane/",
+        },
     },
-  },
+    {
+        imageUrl: "/images/team/sumiko.png",
+        name: "Sumiko Tanaka",
+        role: "CSO",
+        descritpion: `Sumiko manages partnerships and operations to support the mission of advancing AI for all. Her career in international relations began during her undergraduate studies at the University of Washington, leading to a UN internship in New York and a dual master’s degree in diplomacy and strategic negotiations in Paris. She has supported international organizations across Africa, Asia, and Latin America and spent eight years in leadership roles at a company serving global health-focused nonprofits.`,
+        links: {
+            linkedin: "https://www.linkedin.com/in/sumiko-tanaka-pusch/",
+        },
+    }
 ];
 
 /*
