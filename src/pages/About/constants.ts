f098b50c40d10e2b37b854d6f20004710297ff25
Andrés Estévez <aecostas@gmail.com>
2024-12-20 18:49:27 +0100

    wip - about

---
+++ src/pages/About/constants.ts
@@ -0,0 +1,18 @@
+export const values = [
+  {
+    title: "Honesty",
+    text: "We value transparency and integrity, ensuring open communication and trust as the foundation of our collaboration",
+  },
+  {
+    title: "Focused & Fast-Paced",
+    text: "Our work culture prioritizes precision, intensity, and speed, enabling us to develop cutting-edge technology with unmatched rigor",
+  },
+  {
+    title: "Innovative Collaboration",
+    text: "By blending individual responsibility with team synergy, we create a dynamic environment where top talent pushes the boundaries of AI innovation",
+  },
+  {
+    title: "High-Performance",
+    text: "We foster a culture where only the most effective individuals thrive, delivering results that drive meaningful impact",
+  },
+];
