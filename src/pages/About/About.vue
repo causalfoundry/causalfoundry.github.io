633e3f0b7574c196c3da05fc171f6802dc07217c
moizhassankh <62547067+moizhassankh@users.noreply.github.com>
2024-12-23 18:34:52 +0100

    ✨ chore: about culture section

--- src/pages/About/About.vue
+++ src/pages/About/About.vue
@@ -1,28 +1,46 @@
 <template>
   <div class="about">
     <img
-      src="../../assets/gradient-top-yellow.png"
-      class="app__gradient-top-yellow"
+        src="../../assets/gradient-top-yellow.png"
+        class="app__gradient-top-yellow"
     />
     <img
-      src="../../assets/gradient-top-blue.png"
-      class="app__gradient-top-blue"
+        src="../../assets/gradient-top-blue.png"
+        class="app__gradient-top-blue"
     />
     <AppHeader current-page="about">
-      <AppMenu />
+      <AppMenu/>
     </AppHeader>
 
     <PageHeader
-      :description="ABOUT_DESCRIPTION"
-      className="about__header"
-      image="/images/about/top.png"
+        :description="ABOUT_DESCRIPTION"
+        className="about__header"
+        image="/images/about/top.png"
     >
-      <template #title> <AppTitle>AI</AppTitle> for everyone </template>
+      <template #title>
+        <AppTitle>AI</AppTitle>
+        for everyone
+      </template>
     </PageHeader>
+
+    <Section
+        title="Our Culture"
+        description=""
+        className="about-section">
+
+      <div class="values">
+        <div v-for="value of values" class="value" v-bind:key="value.title">
+          <div class="value__title">
+            {{ value.title }}
+          </div>
+          <div class="value__subtitle">{{ value.text }}</div>
+        </div>
+      </div>
+    </Section>
 
     <div class="our-story">
       <div class="our-story__title">
-        Our Story:<br />Revolutionizing Industries with AI
+        Our Story:<br/>Revolutionizing Industries with AI
       </div>
       <div class="our-story__content">
         <div class="our-story__section">
@@ -33,26 +51,26 @@
           advanced machine learning platform, designed to predict and shape
           video game player behavior, marking a significant milestone in the
           gaming world.
-          <br /><br />
+          <br/><br/>
           Following their success in Tokyo, África and Dexian embarked on a new
           challenge at Inditex, the world's largest fashion retailing group and
           the parent company of Zara. Here, they spearheaded AI initiatives,
           blending their deep understanding of machine learning with the
           fast-paced world of fashion retail and supply chain.
-          <br /><br />
+          <br/><br/>
           2020 marked a transformative year for África and Dexian. África and
           Dexian transitioned their focus to a domain with profound global
           impact – AI for global health. Since then, they have been relentless
           in pushing the boundaries of what AI can achieve, especially in
           addressing health.
-          <br /><br />
+          <br/><br/>
           Our journey, from revolutionizing the gaming industry in Tokyo to
           pioneering AI-driven global health initiatives, reflects our
           commitment to harnessing AI for meaningful applications to create a
           better, healthier world.
         </div>
         <div class="our-story__section">
-          <img src="../../assets/our_story.png" />
+          <img src="../../assets/our_story.png"/>
         </div>
       </div>
     </div>
@@ -62,7 +80,7 @@
       <div class="members">
         <div v-for="member of team" class="member">
           <div class="avatar">
-            <img :src="member.imageUrl" alt="" />
+            <img :src="member.imageUrl" alt=""/>
             <div class="description">{{ member.descritpion }}</div>
           </div>
           <div class="info">
@@ -70,31 +88,31 @@
             <div class="role">{{ member.role }}</div>
             <div class="links">
               <a
-                v-if="member.links.linkedin"
-                :href="member.links.linkedin"
-                target="_blank"
+                  v-if="member.links.linkedin"
+                  :href="member.links.linkedin"
+                  target="_blank"
               >
                 <img
-                  src="../../assets/linkedin-about.png"
-                  class="link linkedin"
+                    src="../../assets/linkedin-about.png"
+                    class="link linkedin"
                 />
               </a>
               <a
-                v-if="member.links.twitter"
-                :href="member.links.twitter"
-                target="_blank"
+                  v-if="member.links.twitter"
+                  :href="member.links.twitter"
+                  target="_blank"
               >
                 <img
-                  src="../../assets/twitter-about.png"
-                  class="link twitter"
+                    src="../../assets/twitter-about.png"
+                    class="link twitter"
                 />
               </a>
               <a
-                v-if="member.links.github"
-                :href="member.links.github"
-                target="_blank"
+                  v-if="member.links.github"
+                  :href="member.links.github"
+                  target="_blank"
               >
-                <img src="../../assets/github-about.png" class="link twitter" />
+                <img src="../../assets/github-about.png" class="link twitter"/>
               </a>
             </div>
           </div>
@@ -102,18 +120,18 @@
       </div>
     </div>
 
-    <GetInTouch highlighted />
+    <GetInTouch highlighted/>
     <div class="spacer"></div>
 
-    <AppFooter />
+    <AppFooter/>
 
     <img
-      src="../../assets/gradient-bottom-yellow.png"
-      class="app__gradient-bottom-yellow"
+        src="../../assets/gradient-bottom-yellow.png"
+        class="app__gradient-bottom-yellow"
     />
     <img
-      src="../../assets/gradient-bottom-blue.png"
-      class="app__gradient-bottom-blue"
+        src="../../assets/gradient-bottom-blue.png"
+        class="app__gradient-bottom-blue"
     />
   </div>
 </template>
@@ -126,9 +144,10 @@ import AppTitle from "@/components/AppTitle";
 
 import PageHeader from "@/components/PageHeader";
 
-import { ABOUT_DESCRIPTION } from "./constants";
+import {ABOUT_DESCRIPTION, values} from "./constants";
 import team from "@/data/team";
 
 import "./About.scss";
 import GetInTouch from "@/components/GetInTouch";
+import Section from "@/pages/Product/components/Section/Section.vue";
 </script>
--- src/pages/About/constants.ts
+++ src/pages/About/constants.ts
@@ -18,3 +18,23 @@ user.`;
 export const ABOUT_TITLE = `
 We are a public benefit corporation with a mission to scale
 Reinforcement Learning and advanced AI technology.`;
+
+
+export const values = [
+    {
+        title: "Honesty",
+        text: "We value transparency and integrity, ensuring open communication and trust as the foundation of our collaboration",
+    },
+    {
+        title: "Focused & Fast-Paced",
+        text: "Our work culture prioritizes precision, intensity, and speed, enabling us to develop cutting-edge technology with unmatched rigor",
+    },
+    {
+        title: "Innovative Collaboration",
+        text: "By blending individual responsibility with team synergy, we create a dynamic environment where top talent pushes the boundaries of AI innovation",
+    },
+    {
+        title: "High-Performance",
+        text: "We foster a culture where only the most effective individuals thrive, delivering results that drive meaningful impact",
+    },
+];
