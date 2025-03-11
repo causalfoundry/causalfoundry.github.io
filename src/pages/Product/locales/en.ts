1fd25126f878bbf87587afe23734fe6ff4b191e2
Andrés Estévez <aecostas@gmail.com>
2025-03-11 18:32:27 +0100

    Add i18n for product footer

--- src/pages/Product/locales/en.ts
+++ src/pages/Product/locales/en.ts
@@ -178,5 +178,28 @@ export default {
         ],
       },
     ],
+    footer: {
+      title: "End-to-End Products",
+      description:
+        "Need a tailored app or custom software? Our expert team is here to create the perfect solution for your unique needs",
+      items: [
+        {
+          title: "Designed for AI",
+          icon: "/images/products/e2e/ia.png",
+          text: "Our apps are designed with AI interventions at their core and are integrated with our AI platform, enabling you to unlock actionable insights and personalization at scale",
+        },
+        {
+          title: "Cutting-Edge User Experience",
+          icon: "/images/products/e2e/user.png",
+          text: "We leverage the latest tech stack and modern design principles, all backed by extensive research in user experience, to create intuitive, high-performance tools that drive engagement and efficiency",
+        },
+        {
+          title: "Ready for Interoperability",
+          icon: "/images/products/e2e/interop.png",
+
+          text: "Our solutions support seamless data sharing with other systems, adhering to industry standards for integration and scalability",
+        },
+      ],
+    },
   },
 };
