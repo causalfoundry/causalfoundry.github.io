e9dfb937373f79c30609d2e4b147a693fea87c5f
Andrés Estévez <aecostas@gmail.com>
2025-03-12 15:34:03 +0100

    Take into account i18n in top header links

--- src/components/AppHeader/AppHeader.vue
+++ src/components/AppHeader/AppHeader.vue
@@ -1,47 +1,47 @@
 <template>
   <header class="header">
-    <a href="/">
+    <a :href="`/${currentLang}/`">
       <img src="../../assets/logo.png" class="header__logo" />
     </a>
     <div class="header__links">
-      <router-link to="/products"
-        ><div :class="`header__item ${active === 'products' ? 'active' : ''}`">
+      <router-link :to="`/${currentLang}/products`">
+        <div :class="`header__item ${active === 'products' ? 'active' : ''}`">
           Products
-        </div></router-link
-      >
+        </div>
+      </router-link>
       <!-- Dropdown menu for Cases -->
       <div
-          class="header__item header__dropdown"
-          :class="{ active: active === 'cases' }"
+        class="header__item header__dropdown"
+        :class="{ active: active === 'cases' }"
       >
         <div>Cases</div>
         <div class="dropdown-menu">
-          <router-link to="/cases/healthcare">
+          <router-link :to="`/${currentLang}/cases/healthcare`">
             <div class="dropdown-item">Healthcare</div>
           </router-link>
-          <router-link to="/cases/e-commerce">
+          <router-link :to="`/${currentLang}/cases/e-commerce`">
             <div class="dropdown-item">E-Commerce</div>
           </router-link>
         </div>
       </div>
-      <router-link to="/research"
-        ><div :class="`header__item ${active === 'research' ? 'active' : ''}`">
+      <router-link :to="`/${currentLang}/research`">
+        <div :class="`header__item ${active === 'research' ? 'active' : ''}`">
           Research
-        </div></router-link
-      >
-      <a target="_blank" href="https://docs.causalfoundry.ai/"
-        ><div class="header__item">Docs</div></a
-      >
-      <router-link to="/about"
-        ><div :class="`header__item ${active === 'about' ? 'active' : ''}`">
+        </div>
+      </router-link>
+      <a target="_blank" href="https://docs.causalfoundry.ai/">
+        <div class="header__item">Docs</div>
+      </a>
+      <router-link :to="`/${currentLang}/about`">
+        <div :class="`header__item ${active === 'about' ? 'active' : ''}`">
           About
-        </div></router-link
-      >
-      <router-link to="/careers"
-        ><div :class="`header__item ${active === 'careers' ? 'active' : ''}`">
+        </div>
+      </router-link>
+      <router-link :to="`/${currentLang}/careers`">
+        <div :class="`header__item ${active === 'careers' ? 'active' : ''}`">
           Careers
-        </div></router-link
-      >
+        </div>
+      </router-link>
     </div>
     <div class="header__slot">
       <slot />
@@ -50,6 +50,8 @@
 </template>
 
 <script setup lang="ts">
+import { useRoute } from "vue-router";
+
 const props = defineProps<{
   currentPage:
     | "landing"
@@ -63,4 +65,8 @@ const props = defineProps<{
 }>();
 
 const active = props.currentPage;
+const route = useRoute();
+const currentLang = Array.isArray(route.params.lang)
+  ? route.params.lang[0]
+  : route.params.lang || "en"; // Default to 'en' if no lang is specified
 </script>
