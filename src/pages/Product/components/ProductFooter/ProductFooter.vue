6d2ef33e2cb3fb23353678fc84980855b932f863
Andrés Estévez <aecostas@gmail.com>
2025-03-14 18:14:20 +0100

    Include footer translations in products

--- src/pages/Product/components/ProductFooter/ProductFooter.vue
+++ src/pages/Product/components/ProductFooter/ProductFooter.vue
@@ -2,35 +2,35 @@
   <div class="product-footer">
     <div class="product-footer__container">
       <header>
-        <div class="title">End-to-End Products</div>
+        <div class="title">{{ messages?.product?.footer?.title }}</div>
         <div class="subtitle">
-          Need a tailored app or custom software? Our expert team is here to
-          create the perfect solution for your unique needs
+          {{ messages?.product?.footer?.description }}
         </div>
       </header>
 
       <div class="product-footer__items">
         <div
-          v-for="(item, index) in items"
+          v-for="(item, index) in messages?.product?.footer?.items"
           :key="index"
           class="[`item`, { active: index === activeFeatureIndex }]"
         >
           <img :src="item.icon" />
           <div class="title">{{ item.title }}</div>
-
           <div class="subtitle">{{ item.text }}</div>
         </div>
       </div>
     </div>
     <img
-        src="../../../../assets/pattern-wave.png"
-        class="product-footer__pattern-wave"
-        alt="Wave Pattern"
+      src="../../../../assets/pattern-wave.png"
+      class="product-footer__pattern-wave"
+      alt="Wave Pattern"
     />
   </div>
 </template>
 
 <script setup lang="ts">
 import "./product-footer.scss";
-import { items } from "./constants";
+import { useTranslations } from "@/composables/useTranslations";
+
+const { currentTranslations: messages } = useTranslations("product");
 </script>
