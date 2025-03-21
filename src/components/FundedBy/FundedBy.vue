c024d3aabcb77eecb540d82fd0748dec40a900e4
Andrés Estévez <aecostas@gmail.com>
2025-03-21 16:59:41 +0100

    Use png instead of ssvg for gates picture

--- src/components/FundedBy/FundedBy.vue
+++ src/components/FundedBy/FundedBy.vue
@@ -2,7 +2,7 @@
   <section class="funded-by">
     <div class="funded-by__header">Funded by</div>
     <div class="funded-by__logos">
-      <img src="./assets/gates_foundation.svg" class="funded-by__logo-bill" />
+      <img src="./assets/gates_foundation.png" class="funded-by__logo-bill" />
       <img src="./assets/google.png" class="funded-by__logo-google" />
     </div>
     <img src="../../assets/pattern-wave.png" class="funded-by__pattern-wave" />
