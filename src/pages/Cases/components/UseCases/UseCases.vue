9f0eeecf198b498b65c1940814ce21a27b6c42a8
Andrés Estévez <aecostas@gmail.com>
2025-04-22 10:45:51 +0200

    Title to use cases

--- src/pages/Cases/components/UseCases/UseCases.vue
+++ src/pages/Cases/components/UseCases/UseCases.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="use-cases">
-    <div v-for="(item, index) in items" :key="index" class="use-cases__item">
-      <a :href="item.link" class="use-cases__link">
-        <img :src="item.image" alt="Use case image" class="use-cases__image" />
-      </a>
+  <div class="use-cases-container">
+    <div class="use-cases-container__title">Our use cases</div>
 
-      <div class="use-cases__details">
-        <span class="use-cases__category">{{ item.category }}</span>
-        <span class="use-cases__company">{{ item.company }}</span>
-        <p class="use-cases__text">{{ item.text }}</p>
+    <div class="use-cases">
+      <div v-for="(item, index) in items" :key="index" class="use-cases__item">
+        <a :href="item.link" class="use-cases__link">
+          <img
+            :src="item.image"
+            alt="Use case image"
+            class="use-cases__image"
+          />
+        </a>
+
+        <div class="use-cases__details">
+          <span class="use-cases__category">{{ item.category }}</span>
+          <span class="use-cases__company">{{ item.company }}</span>
+          <p class="use-cases__text">{{ item.text }}</p>
+        </div>
       </div>
     </div>
   </div>
